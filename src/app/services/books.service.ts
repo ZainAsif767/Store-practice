8147d9cc5575060e86da79511f6352bc89760f75
Zain Asif <zainasif7345@gmail.com>
2024-08-07 17:38:13 +0500

    feat(books-api): getBooks API implementation

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Book } from '../models/books.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BooksService {
+export class GoogleBooksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<Array<Book>> {
+    return this.http
+      .get<{ items: Book[] }>(
+        'https://www.googleapis.com/books/v1/volumes?maxResults=5&orderBy=relevance&q=oliver%20sacks'
+      )
+      .pipe(map((books) => books.items || []));
+  }
 }
